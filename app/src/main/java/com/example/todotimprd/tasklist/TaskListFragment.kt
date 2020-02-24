7ee02963817a2fea85ea9f02694e30a15fe06453
PARDIEU Timothé <tp180802@pdts4gk8729.atlas.local>
2020-02-24 14:20:18 +0100

    TaskListFragment

--- app/src/main/java/com/example/todotimprd/tasklist/TaskListFragment.kt
+++ app/src/main/java/com/example/todotimprd/tasklist/TaskListFragment.kt
@@ -1,11 +1,11 @@
-package com.example.todotimothepardieu.tasklist
+package com.example.todotimprd
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.todotimothepardieu.R
+
 
 class TaskListFragment : Fragment() {
 
